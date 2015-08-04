3727bc74c6b089a44227a73f7168d1d84a6f34bf
Ezra Raez <ezRAez10@gmail.com>
2015-08-03 18:49:33 -0700

    added index, show, new, create for actors controller

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,3 +1,29 @@
 class ActorsController < ApplicationController
 
+  def index
+    @actors = Actor.all
+  end
+
+  def show
+  end
+
+  def new
+    @actor = Actor.new
+  end
+
+  def create
+    @actor = Actor.new(actor_params)
+    if @actor.save
+      redirect_to '/actors/index'
+    else
+      render 'new'
+    end
+  end
+
+private
+
+  def actor_params
+      params.require(:actor).permit(:name, :year_of_birth, :thumbnail)
+  end
+
 end
